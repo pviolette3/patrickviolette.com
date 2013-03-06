a38446d1cd1989aea3ad879e5908a327c0f0f29c
Patrick Violette <patrick@ubuntu.(none)>
2013-03-06 14:52:11 -0800

    Basically finished...tags don't show up yet for some reason though...

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,34 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
-urlpatterns = patterns('',
-   url(r'^', ListView.as_view( queryset=Post.objects.all().order_by("-created")[:2],template_name="blog.html")),
+from django.contrib.syndication.views import Feed
+
+class BlogFeed(Feed):
+  title = "Patrick's Blog"
+  description = "My blog posts"
+  link = "/blog/feed"
+
+  def items(self):
+    return Post.objects.all().order_by("-created")
+
+  def item_title(self, post):
+    return post.title
+
+  def item_description(self, post):
+    return post.body
+
+  def item_link(self, post):
+    return u"/blog/%d" % post.id
+
+urlpatterns = patterns('blog.views',
+   url(r'^$', ListView.as_view( 
+     queryset=Post.objects.all().order_by("-created")[:2],
+     template_name="blog.html")),
+   url(r'^(?P<pk>\d*)$', DetailView.as_view(
+     model=Post,template_name="post.html")),
+   url(r'^archive/$',ListView.as_view(
+     queryset=Post.objects.all().order_by("-created"),
+     template_name="archive.html")),
+   url(r'^tag/(?P<tag>\w*)$', 'tags'),
+   url(r'^feed/$', BlogFeed()),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from blog.models import Post
+from django.shortcuts import render_to_response
+
+def tags(request, tag):
+  posts = Post.objects.filter(tags__name=tag)
+  return render_to_response("tags.html", {"posts": posts, "tag": tag})
